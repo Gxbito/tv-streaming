c617702a09f8bd8d430b3120f60a5062011af91b
Gxbito <g4bmz02@gmail.com>
2024-05-27 19:23:05 -0300

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,124 @@
+const articles = [
+    {
+        id: 1,
+        title: "Live Streaming Market Competitive Landscape: Major Players, Competitor Market Shares, And Player Strategies - By The Custom Market Insights",
+        image: "./assets/13.jpg",
+        link:"./article-1.html"
+    },
+    {
+        id: 2,
+        title: "Disney and Warner Bros. Discovery Reinvented Cable Both companies have announced a bundle that combines Disney+, Hulu and Max. This may not be the answer to #DontStreamOnMax that fans want",
+        image: "./assets/1.jpg",
+        link:"./article-2.html"
+    },
+    {
+        id: 3,
+        title: "The number of US streaming TV subscribers unexpectedly rises in late 2023 - now analysts are reconsidering the company's main 'story",
+        image: "./assets/3.jpg",
+        link:"./article-3.html"
+    },
+    {
+        id: 4,
+        title: "Share Analysis - Growth Trends & Media Streaming Market Size & Forecasts (2024 - 2029)",
+        image: "./assets/2.jpg",
+        link:"./article-4.html"
+    },
+    {
+        id: 5,
+        title: "Streaming accounts for a record 38.7% of total TV viewing, with purchased movies outpacing new originals",
+        image: "./assets/14.jpg",
+        link:"./article-5.html"
+    },
+    {
+        id: 6,
+        title: "Netflix has announced plans to bring AdTech into the company by 2024",
+        image: "./assets/4.jpg",
+        link:"./article-6.html"
+    },
+    {
+        id: 7,
+        title: "Navigating the Flowing Seas: Paramount's Bold Steps and Challenges",
+        image: "./assets/5.jpg",
+        link:"./article-7.html"
+    },
+    {
+        id: 8,
+        title: "Popular Products: OBSBOT Tail Air AI PTZ Streaming Camera",
+        image: "./assets/6.jpg",
+        link:"./article-8.html"
+    },
+    {
+        id: 9,
+        title: "Navigate the dynamic world of personalized streaming video advertising",
+        image: "./assets/7.jpg",
+        link:"./article-9.html"
+    },
+    {
+        id: 10,
+        title: "Spotify is ready to compete for video advertising",
+        image: "./assets/10.jpg",
+        link:"./article-10.html"
+    },
+    {
+        id: 11,
+        title: "FUBO retains RSN Bally Sports after multi-year deal",
+        image: "./assets/15.jpg",
+        link:"./article-11.html"
+    },
+    {
+        id: 12,
+        title: "Philo adds 6 new free channels from A&E and more",
+        image: "./assets/16.jpg",
+        link:"./article-12.html"
+    },
+    {
+        id: 13,
+        title: "With the launch of MLS, Apple brings its popular multiview feature to iPad",
+        image: "./assets/17.jpg",
+        link:"./article-13.html"
+    },
+    {
+        id: 14,
+        title: "Community leaves Netflix, but begins filming",
+        image: "./assets/18.jpg",
+        link:"./article-14.html"
+    },
+    {
+        id: 15,
+        title: "The new Sling TV competition pays to watch TV",
+        image: "./assets/19.jpg",
+        link:"./article-15.html"
+    },
+    {
+        id: 16,
+        title: "Roku Enables Forced Two-Factor Authentication for All Roku Owners",
+        image: "./assets/20.jpg",
+        link:"./article-16.html"
+    },
+    {
+        id: 17,
+        title: "DIRECTV introduces new loyalty program benefits",
+        image: "./assets/21.jpg",
+        link:"./article-17.html"
+    },
+    {
+        id: 18,
+        title: "The producer of The King of the Hill says that the debut of the series will not happen until 2025",
+        image: "./assets/22.jpg",
+        link:"./article-18.html"
+    },
+    {
+        id: 19,
+        title: "Philo adds Smithsonian & POP TV to its lineup",
+        image: "./assets/23.jpg",
+        link:"./article-19.html"
+    },
+    {
+        id: 20,
+        title: "Can ESPN+ and Netflix work together? Activist investor Disney believes",
+        image: "./assets/24.jpg",
+        link:"./article-20.html"
+    },
+];
+
+export default articles;
